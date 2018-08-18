1f762c096a6d59ea574dd7fffed3ccb9d40ad126
Stanislaw Karkosz <stakarkosz@gmail.com>
2018-08-18 13:30:05 +0200

    Fixed bugs in new version of preparing dataset module

--- preparing_dataset.py
+++ preparing_dataset.py
@@ -7,59 +7,70 @@ class create_dataset():
     def __init__(self):
         self = self
         self._target = {"8":0,"14":1,"28":2}
-        self.data = np.array()
 
-    def check_path(self):
-        os.chdir('/home/%s' %os.environ['USER'])
+    def go_path(self):
+        """
+        Changes the directory for the one that contains data.
+        """
+        os.chdir('/home/%s/data/' %os.environ['USER'])
         Y = os.listdir()
-        if any(n == "data" for n in Y) == False:
-            print('Path to \'data\' directory not found')
-            path = input('Please enter the path:')
-            os.chdir(path)
-            Y = os.listdir()
-        else:
-            os.chdir('data/')
-        if any(n.startswith('SUBJ') for n in Y) == False:
-            print('Directory with data not found')
-        else:
-            # self._DirectoriesList = [n for n if n.startswith('SUBJ')]
-            self._DirectoriesList = [n.startswith('SUBJ') for n in os.listdir()]
-            print(self._DirectoriesList)
+        self._DirectoriesList = [n for n in Y if n.startswith('SUBJ')]
 
-    def _create_array(path):
-        x = np.load_txt(z,delimiter = ',')
-        re_sult = re.search('(?P<freq>\d)+Hz',z)
+    def _create_array(self,path):
+        """
+        Serves for creating an array from path. In name of path is supposed to be
+        an information about targeted frequency, which is extracted and added in
+        form of additional column.
+        """
+        x = np.loadtxt(path,delimiter = ',')
+        re_sult = re.search('(?P<freq>\d+)Hz',path)
         f = re_sult.group('freq')
         target = self._target[f]
         t = [target for n in range(x.shape[0])]
+        return np.column_stack((x,t))
 
 
-    def create_first_array(self):
-
+    def create_placeholder_array(self):
+        """
+        This method creates a placeholder in attribute data, so one can vertically
+        stack the data forwards. The shape of output data is n_channels + 1
+        (number of channels and 1 column for target).
+        """
         os.chdir(self._DirectoriesList[0])
-
-        X = self._create_array(os.listdir[0])
-        self.data = X
+        X = np.loadtxt(os.listdir()[0],delimiter=',')
+        _x = X.shape[1] + 1
+        self.data = np.zeros(_x)
         os.chdir('../')
 
-    def reading(self):
+    def read_write(self):
+        """
+        This method iterates through all directories with data, reads it then
+        stack every each to data attirbute.
+        """
+        X = self.data
         for n in self._DirectoriesList:
             os.chdir(n)
             for z in os.listdir():
+                print(z)
                 t = self._create_array(z)
-                X = np.column_stack(x,t)
-                self.data = np.vstack(self.data,X)
+                X = np.vstack((X,t))
             os.chdir('../')
-    def run(self):
-        self.check_path()
-        self.create_first_array()
-        self.reading()
+        X = X[1:]
+        self.data = X
 
+    def run(self):
+        """
+        This method runs all modules of this class, then saves the data attribute as
+        dataset.npz.
+        """
+        self.go_path()
+        self.create_placeholder_array()
+        self.read_write()
         np.savez('dataset.npz',self.data)
+
 
 
 # if __name__ == "__main__":
 dc = create_dataset()
-dc.check_path()
-
+dc.run()
 #TODO Figure out how to create first array, complete creating array function
