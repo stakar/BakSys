4b548c2bb2f3c805543d34dbb74c090bdc40f302
Stanislaw Karkosz <stakarkosz@gmail.com>
2018-09-12 09:40:31 +0200

    Changed way of returning the correlation coeficients in corr_extract, added fit and predict functions

--- CCA.py
+++ CCA.py
@@ -37,31 +37,39 @@ class BakSysCCA(object):
 
     def corr_extract(self,data):
 
-        bs = BakSys(extract = self.extract, freq = self.freq,sep = self.sep,
-        channels = self.channels,threeclass=self.threeclass,
-        seconds=self.seconds).fit_transform(data).squeeze()
-        F,P = bs.shape
-
+        F,P = data.shape
         tf = self.target_freq
         cancor = CCA(n_components=1)
+        result = dict()
 
-        result = np.zeros(1)
-        for z in tf:
-            for n in range(F):
-                tmp = bs[n]
-                tmp = np.array([tmp,tmp]).reshape(P,2)
-                ref = np.array(self._reference(z)).reshape(P,2)
-                u,v = cancor.fit_transform(tmp,ref)
-                cro = np.corrcoef(u.T,v.T)[0,1]
-                result = np.vstack((result,np.max(np.abs(cro))))
-        result = result[1:]
+        for z,n in zip(tf,range(F)):
+            tmp = data[n]
+            tmp = np.array([tmp,tmp]).reshape(P,2)
+            ref = np.array(self._reference(z)).reshape(P,2)
+            u,v = cancor.fit_transform(tmp,ref)
+            cro = np.corrcoef(u.T,v.T)[0,1]
+            maxcor = np.max(np.abs(cro))
+            result[maxcor] = z
+
         return result
 
-data = load_data_path('../subject1/sd14Hz1sec/14Hz1sec12prt1trial.csv')
+    def fit(self,data):
 
-# bs = BakSys().fit_transform(data).squeeze()
-# print('Shape of data is: {}'.format(bs.data.shape))
-#
-cca = BakSysCCA()
-print(cca._reference(8)[0].shape)
-print(cca.corr_extract(data = data))
+        X = BakSys(extract = self.extract, freq = self.freq,sep = self.sep,
+        channels = self.channels,threeclass=self.threeclass,
+        seconds=self.seconds).fit_transform(data).squeeze()
+
+        corr_coef = self.corr_extract(X)
+        self._coef = corr_coef
+
+    def predict(self,data):
+        decision = self._coef[max(self._coef.keys())]
+        return decision
+
+
+
+if __name__ == '__main__':
+    data = load_data_path('../subject1/sd14Hz1sec/14Hz1sec12prt1trial.csv')
+    cca = BakSysCCA(threeclass=False,target_freq=[8,14])
+    cca.fit(data)
+    print(cca.predict(data))
